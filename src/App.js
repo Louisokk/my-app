8efdffca1e591f7f97e57090b1bc4fb934b97519
Louisokk <minecraftyunkie@gmail.com>
2021-03-02 16:05:46 +0100

    LR 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from "./templates/components/Container.js"
+import Header from "./templates/components/Header.js"
+import Footer from "./templates/components/Footer.js"
+import Quickmenu from "./templates/components/Quickmenu.js"
+import { Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Header></Header>
+      <Quickmenu></Quickmenu>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>      
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Link to="/Game">
+      <Container></Container>
+      </Link>
+      <Footer></Footer>
     </div>
   );
 }
